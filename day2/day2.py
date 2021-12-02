4a080d0b55fd9b271794d918716fcf536d9ef91f
Nick Michiels <michiels.nick@gmail.com>
2021-12-02 08:51:22 +0100

    Solution for day 2

---
+++ day2/day2.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# https://adventofcode.com/2021/day/2
+
+
+# part 1
+with open("input.txt") as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+    
+hor, vert, depth = 0, 0, 0
+for line in lines:
+    (dir, count) = line.split(' ')
+    count = int(count)
+    if dir=='forward':
+        hor += count
+    if dir=='down':
+        depth += count
+    if dir=='up':
+        depth -= count
+        
+print('Result part 1: ', hor * depth)
+
+    
+# part 1
+hor, vert, depth, aim = 0, 0, 0, 0
+for line in lines:
+    (dir, count) = line.split(' ')
+    count = int(count)
+    if dir=='forward':
+        hor += count
+        depth += (aim * count)
+    if dir=='down':
+        aim += count
+    if dir=='up':
+        aim -= count
+        
+print('Result part 2: ', hor * depth)
+
+    
